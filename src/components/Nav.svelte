7797c91d595e96a8cdedf9e708b2ba0b6954ca5a
weberjavi <weberjavi@gmail.com>
2020-01-03 15:47:05 +0100

    first commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+nav {
+	position: absolute;
+	top: 30px;
+}
+	a {
+		padding: 20px;
+	}
+</style>
+
+<nav>
+
+	<a class:selected='{segment === undefined}' href='.'>home</a>
+	<a class:selected='{segment === "about"}' href='about'>about</a>
+	<a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a>
+
+	<!-- <ul>
+		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
+		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
+
+		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
+	</ul> -->
+</nav>
+
+
